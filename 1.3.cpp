8ab80f9ebcb28960e4aebdfa981d12de2b7ab33f
kulakowskin <kulakowskin@yahoo.com>
2016-05-05 16:16:29 -0400

    1.3

---
+++ 1.3.cpp
@@ -0,0 +1,34 @@
+// Nicole Kulakowski
+// Question 1.3
+
+// Given two strings, write a method to decide if one is a permutation 
+// of the other.
+
+//#include "string"
+#include <iostream>
+#include <algorithm>
+
+
+using namespace std;
+
+
+bool sort(string one, string two){
+	if(one.length() != two.length()){
+		return false;
+	}
+	sort(one[0], one[(one.length()/8)]);
+	sort(two[0], two[(two.length()/8)]);
+	if (one == two)
+	{
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
